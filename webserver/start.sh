fc4351c4c3342360a3be857379f357d2fae528a4
Kevin Svitač <kevin@svit.ac>
2024-01-26 12:50:03 +0000

    Increase gunicorn server timeout, so mods can be compiled

--- webserver/start.sh
+++ webserver/start.sh
@@ -14,10 +14,10 @@ trap handle_sigint SIGINT
 # So cli.py can be imported:
 export PYTHONPATH=../
 
-../.venv/bin/gunicorn flask_app:MHKM_FLASK_APP --bind=unix:./mhk_mods_flask.sock -k sync -m 007 -w 4 &
+../.venv/bin/gunicorn flask_app:MHKM_FLASK_APP --bind=unix:./mhk_mods_flask.sock -k sync -m 007 -w 4 --timeout=300 &
 pid1=$!
 
-../.venv/bin/gunicorn fastapi_app:MHKM_FASTAPI_APP --bind=unix:./mhk_mods_fastapi.sock -k uvicorn.workers.UvicornWorker -m 007 -w 4 &
+../.venv/bin/gunicorn fastapi_app:MHKM_FASTAPI_APP --bind=unix:./mhk_mods_fastapi.sock -k uvicorn.workers.UvicornWorker -m 007 -w 4 --timeout=300 &
 pid2=$!
 
 
